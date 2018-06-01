cb1d96ebe8d4c7d077a521d3808f279496e45c34
wilson1745 <wilson-power0130@hotmail.com>
2018-06-01 16:39:37 +0100

    5 seconds DONE

--- app/src/main/java/e/wilso/async/MainActivity.java
+++ app/src/main/java/e/wilso/async/MainActivity.java
@@ -1,9 +1,11 @@
 package e.wilso.async;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,23 +23,42 @@ public class MainActivity extends AppCompatActivity {
       go1.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-
+            new Job1Task().execute();
          }
       });
 
       go2.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-
+            //new Job1Task().execute();
          }
       });
 
       go3.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-
+            //new Job1Task().execute();
          }
       });
+   }
 
+   class Job1Task extends AsyncTask<Void, Void, Void> {
+
+      @Override
+      protected Void doInBackground(Void... voids) {
+         try {
+            Thread.sleep(5000);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+         return null;
+      }
+
+      @Override
+      protected void onPostExecute(Void aVoid) {
+         super.onPostExecute(aVoid);
+         TextView info = findViewById(R.id.info);
+         info.setText("DONE");
+      }
    }
 }
