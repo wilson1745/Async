df5208fbabd370f42886c609dfa1a45a8f0f3db4
wilson1745 <wilson-power0130@hotmail.com>
2018-06-01 17:27:04 +0100

    job2 & job3

--- app/src/main/java/e/wilso/async/MainActivity.java
+++ app/src/main/java/e/wilso/async/MainActivity.java
@@ -30,14 +30,14 @@ public class MainActivity extends AppCompatActivity {
       go2.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-            //new Job1Task().execute();
+            new Job2Task().execute(3);
          }
       });
 
       go3.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-            //new Job1Task().execute();
+            new Job3Task().execute(6);
          }
       });
    }
@@ -59,6 +59,56 @@ public class MainActivity extends AppCompatActivity {
          super.onPostExecute(aVoid);
          TextView info = findViewById(R.id.info);
          info.setText("DONE");
+      }
+   }
+
+   class Job2Task extends AsyncTask<Integer, Void, Void> {
+
+      @Override
+      protected Void doInBackground(Integer... integers) {
+         try {
+            Thread.sleep(integers[0] * 1000);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+         return null;
+      }
+
+      @Override
+      protected void onPostExecute(Void aVoid) {
+         super.onPostExecute(aVoid);
+         TextView info = findViewById(R.id.info);
+         info.setText("DONE2");
+      }
+   }
+
+   class Job3Task extends AsyncTask<Integer, Integer, Void> {
+
+      TextView info = findViewById(R.id.info);
+
+      @Override
+      protected Void doInBackground(Integer... integers) {
+         for(int i=0; i<integers[0]; i++) {
+            publishProgress(i);
+            try {
+               Thread.sleep(1000);
+            } catch (InterruptedException e) {
+               e.printStackTrace();
+            }
+         }
+         return null;
+      }
+
+      @Override
+      protected void onPostExecute(Void aVoid) {
+         super.onPostExecute(aVoid);
+         info.setText("DONE3");
+      }
+
+      @Override
+      protected void onProgressUpdate(Integer... values) {
+         super.onProgressUpdate(values);
+         info.setText(String.valueOf(values[0]));
       }
    }
 }
